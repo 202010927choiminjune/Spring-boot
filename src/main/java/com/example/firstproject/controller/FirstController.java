1d1faaaec9b008f545bf28a8e5c03e77eedc1566
Choi Minjune <minjune0807@gmail.com>
2024-08-11 11:12:01 +0900

    feat: 컨틀롤러 생성 예시 코드

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,13 @@
+package com.example.firstproject.controller;
+
+//컨트롤러 선언과 동시에 자동으로 임포트
+import org.springframework.stereotype.Controller;
+
+//컨트롤러 선언
+@Controller
+public class FirstController {
+    //메서드 작성
+    public String niceToMeetYou(){
+        return "";
+    }
+}
