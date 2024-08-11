d98ea7381390a0c3e0dc624e4c14f5e5317125c2
Choi Minjune <minjune0807@gmail.com>
2024-08-11 11:27:37 +0900

    feat: greetings.mustache 페이지 반환

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,13 +1,19 @@
 package com.example.firstproject.controller;
 
 //컨트롤러 선언과 동시에 자동으로 임포트
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 //컨트롤러 선언
 @Controller
 public class FirstController {
+    //요청
+    @GetMapping("/hi")
     //메서드 작성
     public String niceToMeetYou(){
-        return "";
+        return "greetings";
     }
 }
+//greetings.mustache파일 반환
