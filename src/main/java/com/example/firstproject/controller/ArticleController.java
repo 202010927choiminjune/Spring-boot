7082745260d5fc6d8469859d4ff9691639f0ab44
Choi Minjune <minjune0807@gmail.com>
2024-08-12 13:09:20 +0900

    init: 폼 데이터를 DTO에 담기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.firstproject.controller;
 
 
+import com.example.firstproject.dto.ArticleForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
@@ -11,7 +13,8 @@ public class ArticleController {
         return "articles/new";
     }
     @PostMapping("/articles/create")
-    public String createArticle(){
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString()); //DTO에 폼 데이터가 잘 담겼는지 확인
         return "";
     }
 }
