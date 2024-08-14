65fd8c6561e3a795faeb6b46ee7a6794f36c1486
Choi Minjune <minjune0807@gmail.com>
2024-08-14 16:52:51 +0900

    init: 리파지터리 만들기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,6 +1,6 @@
 package com.example.firstproject.controller;
 
-
+import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import org.springframework.stereotype.Controller;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+private ArticleRepository articleRepository;
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
@@ -18,6 +19,9 @@ public class ArticleController {
         System.out.println(form.toString()); //DTO에 폼 데이터가 잘 담겼는지 확인
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
+        // 2. 리파지터리로 엔티티를 DB에 저장
+        Article saved = articleRepository.save(article);
+
         return "";
     }
 }
