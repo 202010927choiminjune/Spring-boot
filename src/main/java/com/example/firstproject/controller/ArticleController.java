0c5db38e755b6879114e5ad9df54772e5934ecd1
Choi Minjune <minjune0807@gmail.com>
2024-08-16 15:41:22 +0900

    feat: id를 조호해 데이터 가져오기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
 @Slf4j // 로깅 기능을 위한 어노테이션 추가
 @Controller
 public class ArticleController {
@@ -36,6 +39,10 @@ public class ArticleController {
     @GetMapping("/articles/{id}") //데이터 조회 요청 접수
     public String show(@PathVariable Long id){
         log.info("id = " + id);
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        // 3. 뷰 페이지 반환하기
         return "";
     }
 }
