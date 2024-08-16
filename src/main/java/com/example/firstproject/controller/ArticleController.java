796f837758799e4a98c577e53231c3110637e64c
Choi Minjune <minjune0807@gmail.com>
2024-08-16 15:46:39 +0900

    feat: 모델에 데이터 등록하기

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.example.firstproject.entity.Article;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,11 +38,12 @@ public class ArticleController {
         return "";
     }
     @GetMapping("/articles/{id}") //데이터 조회 요청 접수
-    public String show(@PathVariable Long id){
+    public String show(@PathVariable Long id, Model model){
         log.info("id = " + id);
         // 1. id를 조회해 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
         // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
         // 3. 뷰 페이지 반환하기
         return "";
     }
