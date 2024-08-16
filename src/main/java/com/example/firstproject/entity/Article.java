9ae6b2f39f8e49f9039fe99508e91339eb0c497e
Choi Minjune <minjune0807@gmail.com>
2024-08-16 16:22:07 +0900

    init: 기본 생성자 추가하기

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.web.bind.annotation.GetMapping;
 @AllArgsConstructor
+@NoArgsConstructor // 기본 생성자 추가 어노테이션
 @ToString
 @Entity //엔티티 선언
 public class Article {
@@ -18,6 +20,8 @@ public class Article {
     private String title;
     @Column // content 필드 선언
     private String content;
+
+
 
 
 }
