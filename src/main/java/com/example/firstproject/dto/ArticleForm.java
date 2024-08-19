37dc41573b491102eff3834ea0d3d3199cc02913
Choi Minjune <minjune0807@gmail.com>
2024-08-19 18:39:57 +0900

    init: 엔티티를 DB에 저장하기

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -8,14 +8,11 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class ArticleForm {
+    private Long id; //id필드 추가
     private String title; //제목을 받을 필드
     private String content; // 내용을 받을 필드
     //전송받은 제목과 내용을 필드에 저장하는 생성자 추가
-
-
-
-
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
 }
